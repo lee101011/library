ed191ee1a3f327dbdd709e63e6325d3530ef3bab
lee <lee@me.io>
2024-04-25 16:16:04 -0700

    Add completed project

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function Book(title, author, pages, read = false) {
     this.read = read;
 }
 
-Book.prototype.toggleRead = function() {
+Book.prototype.toggleRead = function () {
     this.read = !this.read;
 }
 
@@ -28,7 +28,7 @@ function addBookToLibrary(book) {
 }
 
 function removeBookFromLibrary(index) {
-    if(index < 0) return;
+    if (index < 0) return;
     myLibrary.splice(index, 1);
     drawBookShelf();
 }
@@ -54,6 +54,7 @@ function drawCard(index, book) {
     const card = document.createElement("div");
     card.classList.add("card");
 
+    // viewable alternative to useing data-attribute
     const id = document.createElement("div");
     id.classList.add("id");
     id.textContent = `${index}`;
@@ -117,8 +118,8 @@ function drawCard(index, book) {
     });
 }
 
-function statusResolver(status, book){
-    if(book.read) {
+function statusResolver(status, book) {
+    if (book.read) {
         status.classList.remove("unread");
         status.classList.add("read");
     } else {
@@ -128,3 +129,49 @@ function statusResolver(status, book){
     status.textContent = `${book.read ? "Yes" : "No"}`;
 }
 
+const add = document.querySelector(".add");
+const form = document.querySelector(".form");
+const panel = document.querySelector(".panel");
+
+let formVisible = false;
+
+add.addEventListener("click", (event) => {
+    toggleForm();
+});
+
+function toggleForm() {
+    if (formVisible) {
+        add.style.marginBottom = "100vh";
+    } else {
+        add.style.marginBottom = "3rem";
+        resetForm();
+    }
+
+    formVisible = !formVisible;
+}
+
+const titleF = document.querySelector("#in-title");
+const authorF = document.querySelector("#in-author");
+const pagesF = document.querySelector("#in-pages");
+const readF = document.querySelector("#in-read");
+
+function resetForm() {
+    titleF.value = "";
+    authorF.value = "";
+    pagesF.value = "";
+    readF.checked = false;
+}
+
+const formButton = document.querySelector("#submit");
+
+formButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const book = new Book(
+        titleF.value,
+        authorF.value,
+        pagesF.value,
+        readF.checked
+    );
+    addBookToLibrary(book);
+    resetForm();
+});
