b0587682e3bbb9b5c2d35fb55fc9399c2fee50cd
Your Name <Your Email>
2024-05-05 17:50:16 -0700

    Add conversion to class object Book

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read = false) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+class Book {
+    constructor(title, author, pages, read = false) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-Book.prototype.toggleRead = function () {
-    this.read = !this.read;
+    toggleRead() {
+        this.read = !this.read;
+    }
 }
 
 const addBook = document.querySelector("#add");
